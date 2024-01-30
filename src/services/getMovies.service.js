ec49aebd26cbd11cf276358f77ccd61ed8e30710
FaishalHilmi <faisalhilmi082@gmail.com>
2024-01-30 21:18:25 +0700

    fix url API

--- src/services/getMovies.service.js
+++ src/services/getMovies.service.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const getMovies = async (callback) => {
   try {
     const response = await axios.get(
-      // "https://api.themoviedb.org/3/movie/popular?api_key=a35f55b831697725b5b0b7255abc3736"
       `${import.meta.env.VITE_REACT_APP_URL_MOVIES}movie/popular${
         import.meta.env.VITE_REACT_APP_API_KEY
       }`
